2b955d0409180c63d9bc4051b63ef6a5123f208e
Muhammad Abdurrohman <mabdurrohman26@dev1.idserve.net>
2026-02-16 20:40:53 +0700

    add log for SET operation

--- internal/server/grpc_rego/grpc_rego.go
+++ internal/server/grpc_rego/grpc_rego.go
@@ -2,6 +2,7 @@ package grpc_rego
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 
@@ -18,6 +19,8 @@ type GRPCRego struct {
 
 func (g *GRPCRego) Set(ctx context.Context, in *proto.KeyValue) (*proto.Status, error) {
 	core.Set(in.Key, in.Value)
+	fmt.Println("set key", in.Key)
+	fmt.Println("set value", in.Value)
 	return &proto.Status{}, nil
 }
 
