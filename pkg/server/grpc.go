8cdded89ceefd141737528f43ee317b2be61b6e1
Muhammad Abdurrohman <mabdurrohman26@dev1.idserve.net>
2026-02-17 19:39:39 +0700

    change error handling to golang idiomatic ways

--- pkg/server/grpc.go
+++ pkg/server/grpc.go
@@ -2,17 +2,12 @@ package server
 
 import (
 	"context"
-	"errors"
 
 	"github.com/socketspace-jihad/rego/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
-)
-
-var (
-	KeyDoesntExists = errors.New("key doesn't exists")
 )
 
 type GRPCRego struct {
@@ -53,19 +48,19 @@ func (g *GRPCRego) Disconnect() error {
 	return g.conn.Close()
 }
 
-func (g *GRPCRego) GetString(key string) (string, error) {
+func (g *GRPCRego) GetString(key string) (*string, error) {
 	val, err := g.grpcConn.Get(context.Background(), &proto.Key{Key: key})
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	if val.Value == nil {
-		return "", KeyDoesntExists
+		return nil, nil
 	}
 	value := &wrapperspb.StringValue{}
 	if err := val.Value.UnmarshalTo(value); err != nil {
-		return "", err
+		return nil, err
 	}
-	return value.Value, nil
+	return &value.Value, nil
 }
 
 func (g *GRPCRego) SetString(key string, value string) error {
@@ -83,19 +78,19 @@ func (g *GRPCRego) SetString(key string, value string) error {
 	return nil
 }
 
-func (g *GRPCRego) GetBool(key string) (bool, error) {
+func (g *GRPCRego) GetBool(key string) (*bool, error) {
 	val, err := g.grpcConn.Get(context.Background(), &proto.Key{Key: key})
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 	if val.Value == nil {
-		return false, KeyDoesntExists
+		return nil, nil
 	}
 	value := &wrapperspb.BoolValue{}
 	if err := val.Value.UnmarshalTo(value); err != nil {
-		return false, err
+		return nil, err
 	}
-	return value.Value, nil
+	return &value.Value, nil
 }
 
 func (g *GRPCRego) SetBool(key string, value bool) error {
@@ -113,19 +108,19 @@ func (g *GRPCRego) SetBool(key string, value bool) error {
 	return nil
 }
 
-func (g *GRPCRego) GetInt(key string) (int64, error) {
+func (g *GRPCRego) GetInt(key string) (*int64, error) {
 	val, err := g.grpcConn.Get(context.Background(), &proto.Key{Key: key})
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 	if val.Value == nil {
-		return 0, KeyDoesntExists
+		return nil, nil
 	}
 	value := &wrapperspb.Int64Value{}
 	if err := val.Value.UnmarshalTo(value); err != nil {
-		return 0, err
+		return nil, err
 	}
-	return value.Value, nil
+	return &value.Value, nil
 }
 
 func (g *GRPCRego) SetInt(key string, value int64) error {
@@ -143,19 +138,19 @@ func (g *GRPCRego) SetInt(key string, value int64) error {
 	return nil
 }
 
-func (g *GRPCRego) GetFloat(key string) (float32, error) {
+func (g *GRPCRego) GetFloat(key string) (*float32, error) {
 	val, err := g.grpcConn.Get(context.Background(), &proto.Key{Key: key})
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 	if val.Value == nil {
-		return 0, KeyDoesntExists
+		return nil, nil
 	}
 	value := &wrapperspb.FloatValue{}
 	if err := val.Value.UnmarshalTo(value); err != nil {
-		return 0, err
+		return nil, err
 	}
-	return value.Value, nil
+	return &value.Value, nil
 }
 
 func (g *GRPCRego) SetFloat(key string, value float32) error {
