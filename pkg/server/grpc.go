c27601847ea1e131d0a5e6d805d7f13912195220
Muhammad Abdurrohman <mabdurrohman26@dev1.idserve.net>
2026-02-17 19:18:35 +0700

    add more type for GET and SET grpc operation

--- pkg/server/grpc.go
+++ pkg/server/grpc.go
@@ -78,3 +78,93 @@ func (g *GRPCRego) SetString(key string, value string) error {
 	}
 	return nil
 }
+
+func (g *GRPCRego) GetBool(key string) (bool, error) {
+	val, err := g.grpcConn.Get(context.Background(), &proto.Key{Key: key})
+	if err != nil {
+		return false, err
+	}
+	if val.Value == nil {
+		return false, errors.New("key doesn't exists")
+	}
+	value := &wrapperspb.BoolValue{}
+	if err := val.Value.UnmarshalTo(value); err != nil {
+		return false, err
+	}
+	return value.Value, nil
+}
+
+func (g *GRPCRego) SetBool(key string, value bool) error {
+	stringValue := &wrapperspb.BoolValue{
+		Value: value,
+	}
+	anyMsg, err := anypb.New(stringValue)
+	if err != nil {
+		return err
+	}
+	_, err = g.grpcConn.Set(context.Background(), &proto.KeyValue{Key: key, Value: anyMsg})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *GRPCRego) GetInt(key string) (int64, error) {
+	val, err := g.grpcConn.Get(context.Background(), &proto.Key{Key: key})
+	if err != nil {
+		return 0, err
+	}
+	if val.Value == nil {
+		return 0, errors.New("key doesn't exists")
+	}
+	value := &wrapperspb.Int64Value{}
+	if err := val.Value.UnmarshalTo(value); err != nil {
+		return 0, err
+	}
+	return value.Value, nil
+}
+
+func (g *GRPCRego) SetInt(key string, value int64) error {
+	stringValue := &wrapperspb.Int64Value{
+		Value: value,
+	}
+	anyMsg, err := anypb.New(stringValue)
+	if err != nil {
+		return err
+	}
+	_, err = g.grpcConn.Set(context.Background(), &proto.KeyValue{Key: key, Value: anyMsg})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *GRPCRego) GetFloat(key string) (float32, error) {
+	val, err := g.grpcConn.Get(context.Background(), &proto.Key{Key: key})
+	if err != nil {
+		return 0, err
+	}
+	if val.Value == nil {
+		return 0, errors.New("key doesn't exists")
+	}
+	value := &wrapperspb.FloatValue{}
+	if err := val.Value.UnmarshalTo(value); err != nil {
+		return 0, err
+	}
+	return value.Value, nil
+}
+
+func (g *GRPCRego) SetFloat(key string, value float32) error {
+	stringValue := &wrapperspb.FloatValue{
+		Value: value,
+	}
+	anyMsg, err := anypb.New(stringValue)
+	if err != nil {
+		return err
+	}
+	_, err = g.grpcConn.Set(context.Background(), &proto.KeyValue{Key: key, Value: anyMsg})
+	if err != nil {
+		return err
+	}
+	return nil
+}
